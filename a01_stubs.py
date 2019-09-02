20e58b37edb8e18378b999f0d2aec65183a4b7c6
AlexMeadors <alexmeadors1@gmail.com>
2019-09-02 00:38:10 -0400

    Added Task 2, made a version for the friend in the room.

--- a01_stubs.py
+++ a01_stubs.py
@@ -49,11 +49,34 @@ else:
 
 ######################################################################
 # (Required) Task 2
-# TODO Ask the user for their friend's birth year
+birthYear = int(input("Ok now call your friend over. Hey buddy, what's your birth year?"))
 
-
-# TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
-
+if birthYear == 2000:
+    print("You're a dragon. Be careful with that morning breath.")
+elif birthYear == 2001:
+    print("You're a snake, the animal, not an insult. This time.")
+elif birthYear == 2002:
+    print("You're a horse, go ride into the sunset.")
+elif birthYear == 2003:
+    print("You're a goat. Not the GOAT unfortunately.")
+elif birthYear == 2004:
+    print("You're a monkey, want a banana?")
+elif birthYear == 2005:
+    print("You're a rooster. Know any good rooster puns?")
+elif birthYear == 2006:
+    print("You're a dog, give a pup a bone.")
+elif birthYear == 2007:
+    print("You're a pig, might want to clean your dorm.")
+elif birthYear == 2008:
+    print("You're a rat. But don't let Vinny hear about it.")
+elif birthYear == 2009:
+    print("You're an ox. I got a funny yoke for you!")
+elif birthYear == 2010:
+    print("You're a tiger, your theme song is going to be messed up with Rocky's. It's too late.")
+elif birthYear == 2011:
+    print("You're a rabbit, gotta say those are some pretty big shoes to fill.")
+else:
+    print("You're either 8 or younger or 20 and older. Just go Google it.")
 
 ######################################################################
 # (Optional) Task 3
